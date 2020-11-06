0da7b0d0cd41559ba95a310f9233bda1bb48c7f9
Felipe Franca <felipefrancafj@gmail.com>
2020-11-05 23:37:52 -0300

    sobre design atualizado

--- script/index.js
+++ script/index.js
@@ -1,36 +1,40 @@
-window.onload = () =>{
-    const scrollToIdOnClick = (event) =>{
+window.onload = () => {
+    const scrollToIdOnClick = (event) => {
         event.preventDefault()
         const tag = event.target
         const id = tag.getAttribute('href')
         const section = document.querySelector(id).offsetTop
-    
+
         window.scroll({
-            top: section ,
+            top: section,
             behavior: "smooth"
         })
     }
     const animationScroll = document.querySelectorAll('a[href^="#"]')
     animationScroll.forEach(link => {
-        link.addEventListener('click',scrollToIdOnClick)
+        link.addEventListener('click', scrollToIdOnClick)
     })
 
 
     let menu = document.getElementById('menu'),
         nav = document.getElementById('nav'),
         menuBg = document.getElementById('menu-bg')
-    menu.addEventListener('click',()=>{
+    menu.addEventListener('click', () => {
         menu.classList.toggle('change')
         nav.classList.toggle('change')
         menuBg.classList.toggle('change-bg')
     })
-        
+
     let tag = document.querySelectorAll('#nav li a')
-    for(var element in tag){
-        tag[element].addEventListener('click',()=>{
+    for (var element in tag) {
+        tag[element].addEventListener('click', () => {
             menu.classList.toggle('change')
             nav.classList.toggle('change')
             menuBg.classList.toggle('change-bg')
         })
     }
-}
+//     let aboutButtom = document.getElementById('about')
+//     aboutButtom.addEventListener('click',()=>{
+//         document.querySelector('.sobre').classList.toggle('sobre')
+//     })
+ }
