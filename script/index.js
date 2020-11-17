dfa7c1357b5bc68a23ea362c4a9e2486e543d220
lpj <lpj@DESKTOP-LPBJ548.SrJamal>
2020-11-17 11:57:56 -0300

    novo site responsivo

--- script/index.js
+++ script/index.js
@@ -19,17 +19,43 @@ window.onload = () => {
     nav = document.getElementById("nav"),
     menuBg = document.getElementById("menu-bg");
   menu.addEventListener("click", () => {
-    menu.classList.toggle("change");
-    nav.classList.toggle("change");
-    menuBg.classList.toggle("change-bg");
+    menu.classList.toggle("is-active");
+    console.log('Clicado');
+    // nav.classList.toggle("is-active");
+    // menuBg.classList.toggle("change-bg");
   });
 
-  let tag = document.querySelectorAll("#nav li a");
-  for (var element in tag) {
-    tag[element].addEventListener("click", () => {
-      menu.classList.toggle("change");
-      nav.classList.toggle("change");
-      menuBg.classList.toggle("change-bg");
-    });
-  }
+  // let tag = document.querySelectorAll("#nav li a");
+  // for (var element in tag) {
+  //   tag[element].addEventListener("click", () => {
+  //     menu.classList.toggle("change");
+  //     nav.classList.toggle("change");
+  //     menuBg.classList.toggle("change-bg");
+  //   });
+  // }
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
 };
